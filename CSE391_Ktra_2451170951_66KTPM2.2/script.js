1ddc33d5e0466a57f76ae1d7b4db812e3f084721
ThuyTrang <thuytrang2906006@gmail.com>
2026-06-06 11:19:10 +0700

    up data json

--- CSE391_Ktra_2451170951_66KTPM2.2/script.js
+++ CSE391_Ktra_2451170951_66KTPM2.2/script.js
@@ -1,16 +1,17 @@
 const taskInput = document.getElementById("taskInput");
 const taskError = document.getElementById("taskError");
 
+//hàm mở modal
 function openModal() {
   document.getElementById("modalOverlay").classList.add("show");
 }
-
+//dong model
 function closeModal() {
   document.getElementById("modalOverlay").classList.remove("show");
   clearError();
   taskInput.value = "";
 }
-
+//hàm add
 function submitTask() {
   const taskName = taskInput.value.trim();
 
@@ -23,6 +24,53 @@ function submitTask() {
     alert("Thêm Task thành công: " + taskName);
     closeModal();
   }
+  // lấy danh sách task
+  const taskContainer = document.getElementById("taskContainer");
+
+  // tạo thẻ task mới
+  const taskCard = document.createElement("article");
+
+  taskCard.className = "card-task";
+
+  taskCard.innerHTML = `
+    <div class="card-body">
+      <div class="task-name">
+        <small class="text-muted">Task</small>
+        <span class="fw-bold">${taskName}</span>
+      </div>
+
+      <div class="task-priority">
+        <small class="text-muted">Priority</small>
+        <span class="fw-bold text-danger">High</span>
+      </div>
+
+      <div>
+        <span class="badge bg-light text-dark border px-3 py-2 rounded-pill">
+          To Do
+        </span>
+      </div>
+
+      <div>
+        <button class="btn icon-btn me-2">&#128221;</button>
+
+        <button
+          class="btn icon-btn text-danger"
+          onclick="deleteTask(this)"
+        >
+          &#128465;
+        </button>
+      </div>
+    </div>
+  `;
+
+  // thêm vào danh sách
+  taskContainer.appendChild(taskCard);
+
+  // reset form
+  taskInput.value = "";
+
+  closeModal();
+
 }
 
 function showError(message) {
